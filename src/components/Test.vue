3d1ae4fbf18572791397acfd4de9181b74cde0ef
Dmitry Zudochkin <dnd.pliz@gmail.com>
2019-02-27 22:55:57 +0300

    Initial commit

---
+++ src/components/Test.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="test">
+    <h3>Some indicator</h3>
+
+    <h5>numbers/zip</h5>
+
+    <input type="text" v-model="answer" @keyup.enter="checkAnswer">
+
+    <p>Right answers: {{ rightAnswers }}</p>
+    <p>Wrong answers: {{ wrongAnswers }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Test",
+  data() {},
+  methods: {
+    checkAnswer() {
+      console.log("check answer triggered");
+    }
+  }
+};
+</script>
